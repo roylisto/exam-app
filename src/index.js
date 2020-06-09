dfd0dc8f908a200f0ccb8ba711126fb60662d4ce
Mohammad Sulthan <muhammadsultana@outlook.com>
2020-06-09 17:31:59 +0700

    first init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue';
 import App from './containers/App.vue';
+import Button from 'ant-design-vue/lib/button';
+import 'ant-design-vue/dist/antd';
 import store from './store';
+
+Vue.component(Button.name, Button);
+
+Vue.config.productionTip = false;
 
 new Vue({
   store,
