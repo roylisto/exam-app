da81755ae23a804a333f31c03bd04b6938f47928
Mohammad Sulthan <muhammadsultana@outlook.com>
2020-06-15 15:44:00 +0700

    fix router file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import App from './containers/App.vue';
-import Buefy from 'buefy'
-import 'buefy/dist/buefy.css'
-import route from './router.js';
-import store from './store';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "./containers/App.vue";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
+import router from "./router.js";
+import store from "./store";
 
 Vue.component(Buefy);
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  route,
+  router,
   store,
-  el: '#root',
-  render: h => h(App),
+  el: "#root",
+  render: (h) => h(App),
 });
