70295f98411151c8cf1295c164a618c29f44d1e0
Mohammad Sulthan <muhammadsultana@outlook.com>
2020-06-16 16:25:01 +0700

    add page layout

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import App from "./containers/App.vue";
 import Buefy from "buefy";
-import "buefy/dist/buefy.css";
+// import "buefy/dist/buefy.css";
 import router from "./router.js";
 import store from "./store";
 
-Vue.component(Buefy);
+Vue.use(Buefy);
 Vue.use(VueRouter);
 
 Vue.config.productionTip = false;
