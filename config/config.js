d4ecda052dde809d7c56f4a76364fba7028d6fdc
tokanggit <tokang.dev@gmail.com>
2020-06-17 13:27:53 +0700

    add logging options

--- config/config.js
+++ config/config.js
@@ -5,23 +5,23 @@ module.exports = {
     "password": process.env.DB_PASSWORD_DEVELOPMENT,
     "database": process.env.DB_DATABASE_DEVELOPMENT,
     "host": process.env.DB_HOST_DEVELOPMENT,
-    "dialect": "mysql"
-    
+    "dialect": "mysql",
+    "logging": console.log,
   },
   "staging": {
     "username": process.env.DB_USERNAME_STAGING,
     "password": process.env.DB_PASSWORD_STAGING,
     "database": process.env.DB_DATABASE_STAGING,
     "host": process.env.DB_HOST_STAGING,
-    "dialect": "mysql"
-    
+    "dialect": "mysql",
+    "logging": false
   },
   "production": {
     "username": process.env.DB_USERNAME_PRODUCTION,
     "password": process.env.DB_PASSWORD_PRODUCTION,
     "database": process.env.DB_DATABASE_PRODUCTION,
     "host": process.env.DB_HOST_PRODUCTION,
-    "dialect": "mysql"
-    
+    "dialect": "mysql",
+    "logging": false
   }
 };
