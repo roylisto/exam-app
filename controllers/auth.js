66e946bbbf247dd8794a9e9403de956cf79a4da5
pejuangbitco <irsyadeinstein@gmail.com>
2020-06-17 09:31:29 +0700

    add package md5, valid date password commented

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,34 +1,45 @@
 const { peserta } = require('../models/index.js');
 const jwt = require('jsonwebtoken');
+// const { Op } = require('sequelize');
+const moment = require('moment');
+moment.defaultFormat = "YYYY-MM-DD HH:mm:ss";
 
 module.exports = {
-  login: async (req, res) => {  
+  login: (req, res) => {  
     let { email, password } = req.body 
-    console.log(email)     
+    // let today = moment().format(); 
+    // let tomorrow = moment().add(1, 'days').format();
+
     peserta.findOne({
       where: {
         email: email,
-        password: password
+        password: password,
+        // valid: {
+        //   [Op.gte]: today
+        // },
+        // expired: {
+        //   [Op.lte]: tomorrow
+        // }
       }
     }).then(peserta => {
-      if(peserta === null) {
+      if(peserta) {        
+        //sign token for 12 hour
+        jwt.sign({ data:peserta }, process.env.JWT_SECRET, { algorithm:'HS256', expiresIn:'12h' }, (error, token) => {        
+          res.json({
+            status: 'OK',
+            message: 'Login Success',
+            data: {
+              token: token
+            }
+          });  
+        });
+      } else {
         res.status(401).json({
           status: 'ERROR',
           message: 'Username atau Password Salah!',
           data: {}
         });
       }
-      
-      //sign token for 24 hour
-      jwt.sign({ data:peserta }, process.env.JWT_SECRET, { algorithm:'HS256', expiresIn:'24h' }, (err, token) => {        
-        res.json({
-          status: 'OK',
-          message: 'Login Success',
-          data: {
-            token: token
-          }
-        });  
-      });
     }).catch (err => {      
       res.status(400).json({
         status: 'ERROR',
--- seeders/20200616175236-peserta.js
+++ seeders/20200616175236-peserta.js
@@ -5,18 +5,27 @@ module.exports = {
     return queryInterface.bulkInsert('peserta', [{      
       email: 'jhon@gmail.com',
       password: '12345678',
-            
+      valid: new Date(),
+      expired: new Date(),
+      jadwal_test: 2
     }, {
       email: 'syadeins@gmail.com',
       password: '87654321',
-            
+      valid: new Date(),
+      expired: new Date(),
+      jadwal_test: 2
     }, {
       email: 'bebas@gmail.com',
       password: '12341234',     
+      valid: new Date(),
+      expired: new Date(),
+      jadwal_test: 1
     }, {
       email: 'anasaya@gmail.com',
       password: '43214321',
-            
+      valid: new Date(),
+      expired: new Date(),
+      jadwal_test: 2
     }], {});
   },
 
