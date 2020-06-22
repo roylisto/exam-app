35bbc7cc772f29b4aad7fe1a2ce7ca93fe591a8e
Roylisto <roylisto.perdana@finaccel.co>
2020-06-22 15:06:09 +0700

    fix router.js

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from "../src/views/Home.vue";
-import Login from "../src/views/login.vue";
+import Login from "../src/views/Login.vue";
 import RincianTest from "../src/views/RincianTest.vue";
 import PetunjukSoal from "../src/views/PetunjukSoal.vue";
 
