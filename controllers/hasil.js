bbc862eda2ce5dd62ee8a11c74f57e9afa18a106
tokanggit <tokang.dev@gmail.com>
2020-07-22 00:34:16 +0700

    add files folder when application start

--- controllers/hasil.js
+++ controllers/hasil.js
@@ -171,7 +171,7 @@ module.exports = {
         worksheet2.addRow(row2);
       }
 
-      let nameFile = `${event_test.waktu}_${event_test.instansi}`;
+      let nameFile = `${moment(event_test.waktu).format('YYYY-MM-DD')}_${event_test.instansi}`;
       nameFile = nameFile.replace(/ /g,"_").replace(/:/g,"-") +'-'+event_test.id;
 
       await workbook.xlsx.writeFile(`./files/${nameFile}.xlsx`);
